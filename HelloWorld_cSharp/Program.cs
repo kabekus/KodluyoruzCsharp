6138227e9f33ee966d11d5f70dfcd75d2ffe88bc
Kabe KUŞ <kabekus.123@gmail.com>
2021-07-15 04:35:11 +0300

    Hello World! with CSharp

---
+++ HelloWorld_cSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld_cSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("First Name:");
+            string firstName= Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            string lastName= Console.ReadLine();
+            Console.WriteLine("Merhaba " + firstName + " " + lastName);
+        }
+    }
+}
