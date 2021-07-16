5aedaaff63a31dad9812a47d532f9328d9746d8c
Kabe KUŞ <kabekus.123@gmail.com>
2021-07-17 00:12:03 +0300

    Tür Dönüşümleri Çalışmaları

---
+++ TurDonusum_cSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TurDonusum_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tür dönüşümleri : Implicit Conversion(Bilinçsiz) - Explicit Conversion(Bilinçli)
+
+            //Implicit Conversion(Bilinçsiz)
+            float a; int b = 25; a = b;
+            Console.WriteLine(a);
+            //Kapasite ksıtlamasına göre çeşitlendirilebilir
+
+            //Explicit Conversion(Bilinli)
+               
+            int c; c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(c);
+            int p; p = int.Parse(Console.ReadLine());
+            Console.WriteLine(p);
+            long l = long.Parse(Console.ReadLine());
+            Console.WriteLine(l);
+            int xx=5;
+            string yy = xx.ToString();
+            Console.WriteLine("yy: "+ yy);
+
+
+
+
+        }
+    }
+}
