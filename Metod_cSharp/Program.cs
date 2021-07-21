9ed0c483d85158bacf74aeebc1c178f91b707b0a
Kabe KUŞ <kabekus.123@gmail.com>
2021-07-21 04:57:59 +0300

    Metodlar 1. Ders

---
+++ Metod_cSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metod_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2, b=9; 
+            Console.WriteLine(a+b);
+
+            int result = Gather(a,b);
+            Console.WriteLine(result);
+
+            Operation example = new Operation();
+            example.WriteToScreen(Convert.ToString(result));
+            
+            int result2 = example.IncreaseAndCollect(ref a , ref b);
+            example.WriteToScreen(Convert.ToString(result2));
+            example.WriteToScreen(Convert.ToString(a+b));
+
+        }
+        static int Gather(int x, int y){
+            return x+y;
+        }
+    }
+    class Operation
+    {
+        public void WriteToScreen(string value){
+            Console.WriteLine(value);
+        }
+
+        public int IncreaseAndCollect(ref int x, ref int y){
+            x += 1;
+            y += 1;
+            return x+y;
+
+        } 
+
+    }
+}
